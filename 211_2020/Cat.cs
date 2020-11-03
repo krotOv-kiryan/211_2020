d39619ad3c0a838964d30e8ba272b650e8856ce1
krotOv-kiryan <kirya.krotov.03@mail.ru>
2020-11-03 13:59:12 +1000

    Добавьте файлы проекта.

---
+++ 211_2020/Cat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _211_2020
+{
+    class Cat
+    {
+        public Cat(string name, DateTime berthday)
+        {
+
+            Name = name;
+            Berthday = berthday;
+
+
+        }
+
+        public string Name
+        {
+            get;
+
+            set;
+        }
+        public void MakeNoise()
+        {
+
+            Console.WriteLine($"{Name} мяукает");
+        }
+        public DateTime Berthday
+        {
+            get;
+
+            set;
+
+        }
+        public int GetAge()
+        {
+            return (DateTime.Today - Berthday).Days / 365;
+        }
+    }
+}
---
+++ 211_2020/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _211_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+у меня не получилось сделать так,чтобы мы сами в консоли
+вводили произвольное имя и день рождения кота,поэтому я
+просто его задам - Kot(я не понимаю,как это реализовать) */
+
+            Cat c = new Cat("Kot", new DateTime(2001, 01, 01));
+            c.MakeNoise();
+            Console.WriteLine($"Коту {c.Name} {c.GetAge()} лет");
+
+            double n = 0;
+            Console.WriteLine($"Программа сделана правильно?(1= да, 2= нет");
+            double.TryParse(Console.ReadLine(), out n);
+            if (n == 1)
+            {
+                Console.WriteLine($"Спасибо.");
+            }
+            if (n == 2)
+            {
+                Console.WriteLine($"Ладно.");
+            }
+        }
+    }
+}
